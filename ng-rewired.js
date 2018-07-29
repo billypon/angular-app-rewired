d12623f5db748bbb69fcccaee2f6faf63ee1022b
billypon <billypon@gmail.com>
2018-07-29 22:51:08 +0800

    add exmple config

---
+++ ng-rewired.js
@@ -0,0 +1,22 @@
+module.exports = {
+  getBrowserConfig: function (options) {
+  },
+
+  getCommonConfig: function (options) {
+  },
+
+  getDevConfig: function (options) {
+  },
+
+  getProdConfig: function (options) {
+  },
+
+  getServerConfig: function (options) {
+  },
+
+  getStylesConfig: function (options) {
+  },
+
+  getTestConfig: function (options) {
+  }
+}
