4e8a5c4ad8d349f4a0f6e89fde504d0bf3825524
billypon <billypon@gmail.com>
2018-07-29 22:45:18 +0800

    check if can find webpack config

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+var fs = require('fs');
+
 var find = require('module-find')();
 var angular = find('@angular/cli');
 
@@ -9,7 +11,13 @@ var cmd = args[0];
 var args = args.slice(1);
 
 var rewiredConfig = process.cwd() + '/ng-rewired.js';
-if (require('fs').existsSync(rewiredConfig)) {
+var webpackConfigs = angular && angular + '/models/webpack-configs';
+
+if (!fs.existsSync(webpackConfigs)) {
+  throw new Error('can\'t find webpack config of angular cli');
+}
+
+if (fs.existsSync(rewiredConfig)) {
   switch (cmd) {
     case 'start':
     case 'build':
@@ -17,7 +25,7 @@ if (require('fs').existsSync(rewiredConfig)) {
     case 'test':
     case 'e2e':
       rewiredConfig = require(rewiredConfig);
-      var webpackConfigs = require(angular + '/models/webpack-configs');
+      webpackConfigs = require(webpackConfigs);
       for (var x in webpackConfigs) {
         if (rewiredConfig[x]) {
           rewireWebpackConfig(webpackConfigs, x);
